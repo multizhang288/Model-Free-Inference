7dee942e278ae864af59a992a13964aada1d0864
multizhang288 <multizhang@gmail.com>
2023-05-13 18:23:56 -0400

    Upload

--- Study 2/Transformation/Simulation_Transformation.R
+++ Study 2/Transformation/Simulation_Transformation.R
@@ -6,9 +6,9 @@
 root_path = dirname(getwd())
 source(paste(root_path,"/Function-Study2.R",sep=""))
 #####
-repeatnum = 10
+repeatnum = 1000
 n=n1= 200
-p=40
+p=2000
 CovMatrix = matrix(rep(0,p^2),p,p)
 for(i in 1:p)
 {
@@ -54,10 +54,10 @@ for(q in 1:repeatnum)
     h1 = Result0$h
     d1 = Result0$d
     Result = c(q,hfix,n,model,delta,Dist,Trans,active,s,d1,h1,penalty,Type1,Type2)
-    print(c(q,Trans,time1-time0,Type1))
-#    write.csv(Result,paste("Impact_Model_",model,"_n_",n,"_h_",h1,"_s_",s,"_d_",d1,"_delta_",delta,"_Dist_",Dist,"_Trans_",Trans,"_active_",active,"_end.csv",sep=""),row.names = FALSE,col.names = FALSE)
-#    write.csv(Record0,paste("Pvalue_Model_",model,"_n_",n,"_h_",h1,"_s_",s,"_d_",d1,"_delta_",delta,"_Dist_",Dist,"_Trans_",Trans,"_active_",active,"_end.csv",sep=""),row.names = FALSE,col.names = FALSE) 
-#    write.csv(Record1,paste("Stat_Model_",model,"_n_",n,"_h_",h1,"_s_",s,"_d_",d1,"_delta_",delta,"_Dist_",Dist,"_Trans_",Trans,"_active_",active,"_end.csv",sep=""),row.names = FALSE,col.names = FALSE)
+    print(c(q,Dist,time1-time0,Type1))
+    write.csv(Result,paste("Impact_Model_",model,"_n_",n,"_h_",h1,"_s_",s,"_d_",d1,"_delta_",delta,"_Dist_",Dist,"_Trans_",Trans,"_active_",active,"_end.csv",sep=""),row.names = FALSE,col.names = FALSE)
+    write.csv(Record0,paste("Pvalue_Model_",model,"_n_",n,"_h_",h1,"_s_",s,"_d_",d1,"_delta_",delta,"_Dist_",Dist,"_Trans_",Trans,"_active_",active,"_end.csv",sep=""),row.names = FALSE,col.names = FALSE) 
+    write.csv(Record1,paste("Stat_Model_",model,"_n_",n,"_h_",h1,"_s_",s,"_d_",d1,"_delta_",delta,"_Dist_",Dist,"_Trans_",Trans,"_active_",active,"_end.csv",sep=""),row.names = FALSE,col.names = FALSE)
     
   },error=function(e){cat("ERROR","\n")})
 }
